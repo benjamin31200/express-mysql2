46b1f80aedb7c322057e13bcb1ded3e09ede0b17
benjamin31200 <benjamindeneux@hotmail.com>
2022-07-23 20:49:02 +0200

    continuation d'express avec postman

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const con = require('./db-config');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const port = process.env.PORT ?? 3000;
 
@@ -12,18 +13,57 @@ con.connect((err) => {
   }
 });
 
-con.promise().query('SELECT * FROM movies')
-  .then( ([rows,fields]) => {
-    app.get('/api/movies', (req, res) => {
-      if (res) {
-        res.status(200).json(rows);
-      }
-    });
+  con.promise().query('SELECT * FROM movies')
+ .then(([rows,fields]) => {
+  app.get('/api/movies', (req, res) => {
+    if (res) {
+      res.status(200).json(rows);
+    }
   })
-  .catch(console.log)
-  .then( () => con.end());
+ }).catch((err) => {
+     console.warn('error in the request');
+ });
 
+  app.post('/api/movies', (req, res) => {
+    const { title, director, year, color, duration } = req.body;
+    con.query(
+      'INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)',
+      [title, director, year, color, duration],
+      (err, result) => {
+        if (err) {
+          res.status(500).send('Error saving the movie');
+        } else {
+          res.status(200).send('Movie successfully saved');
+        }
+      }
+    );
+  });
 
+  con.promise().query('SELECT * FROM users')
+  .then(([rows,fields]) => {
+   app.get('/api/users', (req, res) => {
+     if (res) {
+       res.status(200).json(rows);
+     }
+   })
+  }).catch((err) => {
+      console.warn('error in the request');
+  });
+
+  app.post('/api/users', (req, res) => {
+    const { firstname, lastname, email } = req.body;
+    con.query(
+      'INSERT INTO users(firstname, lastname, email) VALUES (?, ?, ?)',
+      [firstname, lastname, email],
+      (err, result) => {
+        if (err) {
+          res.status(500).send('Error saving the user');
+        } else {
+          res.status(200).send('User successfully saved');
+        }
+      }
+    );
+  });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
